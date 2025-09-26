1acdde2112db8c941a4e62e15db4e220a42e9e5c
Johan Bülow <johan.bulow@ericsson.com>
2025-09-26 16:05:41 +0200

    More videos.

--- getline.c
+++ getline.c
@@ -1,6 +1,22 @@
 #include <stdio.h> 
+#include <stdlib.h> 
+#include <assert.h> 
+#include <malloc.h> 
+
 
 int main(void) 
 {
+	FILE *file = fopen("dollar.c", "r");
+	assert(file != NULL);
+	
+	char *line = NULL;
+	size_t line_buffer_len = 0;
+	while (getline(&line, &line_len, file) != -1)
+	{
+		printf("line='%s'\n", line);
+	}
+	free(line);
+	assert(feof(file));
+	fclose(file);
 	return 0;
 }
